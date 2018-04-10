10eab4df46b11608fa212d887b38d50de1436473
yuwoquan <841453858@qq.com>
2018-04-10 11:53:31 +0800

    底部菜单栏的搭建

--- app/src/main/java/com/example/administrator/jiayan_project/MainActivity.java
+++ app/src/main/java/com/example/administrator/jiayan_project/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.administrator.jiayan_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.jiayan_project.ui.base.BaseActivity;
+import com.example.administrator.jiayan_project.ui.base.BaseFragment;
+import com.example.administrator.jiayan_project.ui.fragment.main.MainFragment;
+
+public class MainActivity extends BaseActivity{
+
+
+    @Override
+    protected int getContextViewId() {
+        return R.id.qmuidemo;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        if (savedInstanceState == null) {
+            BaseFragment fragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(getContextViewId(), fragment, fragment.getClass().getSimpleName())
+                    .addToBackStack(fragment.getClass().getSimpleName())
+                    .commit();
+        }
     }
 }
